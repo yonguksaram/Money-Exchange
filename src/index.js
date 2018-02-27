bd3cf8b34f288672b41a75e5bb45b65f4e1ee927
Stanislau Alimpau <alimberg@list.ru>
2018-02-27 16:30:08 +0300

    exchange/feature: adding code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+    let money = {};
+    money.H = Math.floor(currency/50);
+    money.Q = Math.floor((currency-money.H*50)/25);
+    money.D = Math.floor((currency-money.H*50-money.Q*25)/10);
+    money.N = Math.floor((currency-money.H*50-money.Q*25-money.D*10)/5);
+    money.P = Math.floor(currency%5);
+    for (let item in money) {
+      if (money[item]<=0) {delete money[item]}
+    };
+    return money;
+    
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 }
