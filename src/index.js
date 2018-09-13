e90a79b594b2b5c78fb98c898d8f6681f5f991b7
Stanislau Alimpau <alimberg@list.ru>
2018-09-14 00:47:32 +0300

    money-exchange/adding code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+  
     let money = {};
-    money.H = Math.floor(currency/50);
-    money.Q = Math.floor((currency-money.H*50)/25);
-    money.D = Math.floor((currency-money.H*50-money.Q*25)/10);
-    money.N = Math.floor((currency-money.H*50-money.Q*25-money.D*10)/5);
-    money.P = Math.floor(currency%5);
-    for (let item in money) {
-      if (money[item]<=0||currency>10000) {delete money[item]}
+    if (currency>10000) {return {error: "You are rich, my friend! We don't have so much coins for exchange"}};
+    if (currency<0) {return {}};
+    let coins = {
+      H:50, 
+      Q:25, 
+      D:10, 
+      N:5, 
+      P:1
     };
-    if (currency>10000) {money.error = "You are rich, my friend! We don't have so much coins for exchange"}
+    for (var key in coins) {
+      money[key] = Math.floor(currency/coins[key]);
+      money[key] == 0 ? delete money[key] : currency = currency%coins[key];
+    };
+
     return money;
-    
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
 }
